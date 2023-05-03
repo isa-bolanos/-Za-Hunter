813dcac0a8fe8cf1060f31127e12a277f76a1d2a
isa-bolanos <isa.bolanos55@gmail.com>
2023-05-03 10:40:51 -0500

    Initial Commit

---
+++ 'Za Hunter/_Za_HunterApp.swift
@@ -0,0 +1,17 @@
+//
+//  _Za_HunterApp.swift
+//  'Za Hunter
+//
+//  Created by isa bolanos on 5/3/23.
+//
+
+import SwiftUI
+
+@main
+struct _Za_HunterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
